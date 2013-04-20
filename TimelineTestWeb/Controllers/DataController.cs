4c64010841c082d8553fecc3b671fc281863455d
Alen Siljak <alen.siljak@gmail.com>
2013-04-20 20:21:06 +0200

    project layout; data controller; main view

---
+++ TimelineTestWeb/Controllers/DataController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class DataController : Controller
+    {
+        //
+        // GET: /Data/
+
+        public ActionResult Index()
+        {
+            Response.ContentType = "text/xml";
+
+            var events = GetDummyEvents();
+            var doc = CreateXDocumentFor(events);
+            var model = getStringFromXDocument(doc);
+
+            // return View(model);
+            return Content(model, "text/xml");
+        }
+
+        private string getStringFromXDocument(XDocument doc)
+        {
+            var result = printSeparately(doc);
+            // var result = approachOne(doc);
+
+            return result;
+        }
+
+        private string approachTwo(XDocument doc)
+        {
+            var wr = new StringWriter();
+
+            doc.Save(wr);
+
+            var settings = new XmlWriterSettings
+            {
+                Encoding = Encoding.UTF8,
+                ConformanceLevel = ConformanceLevel.Document,
+                Indent = true
+            };
+
+            using (XmlWriter writer = XmlWriter.Create(wr, settings))
+            {
+                doc.Save(writer);
+            }
+
+            var result = wr.GetStringBuilder().ToString();
+
+            return result;
+        }
+
+        private string approachOne(XDocument doc)
+        {
+            var stream = new MemoryStream();
+            var writer = new XmlTextWriter(stream, new UTF8Encoding());
+
+            doc.Save(writer);
+
+            writer.Flush();
+            stream.Position = 0;
+
+            // read from stream.
+            var reader = new StreamReader(stream);
+            var result = reader.ReadToEnd();
+
+            reader.Close();
+            writer.Close();
+
+            return result;
+        }
+
+        private string printSeparately(XDocument doc)
+        {
+            return doc.Declaration.ToString() + Environment.NewLine + doc.ToString();
+        }
+
+        private XDocument CreateXDocumentFor(IEnumerable<Event> events)
+        {
+            // var eventNodes = new XElement()
+            var doc = new XDocument(
+                new XDeclaration("1.0", "utf-8", null),
+                new XElement("data",
+                    from e in events
+                    select new XElement("event",
+                        new XAttribute("start", e.Start.ToString("MM\\/dd\\/yyyy hh:mm:ss tt")),
+                        new XAttribute("title", e.Title)
+                        )
+                    )
+                );
+
+            return doc;
+        }
+
+        private List<Event> GetDummyEvents()
+        {
+            var events = new List<Event>();
+
+            events.Add(new Event
+                {
+                    Start = DateTime.Now,
+                    End = DateTime.Now.AddMinutes(15),
+                    Title = "Blah"
+                });
+
+            return events;
+        }
+    }
+}
---
+++ TimelineTestWeb/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Event
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Title { get; set; }
+    }
+}
